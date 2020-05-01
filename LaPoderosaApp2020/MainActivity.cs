b4a466ad1bb4dde3165e04613e99409a0c6b35ff
nicaragua6002 <nicaragua_6002@yahoo.es>
2020-05-01 02:18:18 -0600

    Segunda Parte

--- LaPoderosaApp2020/MainActivity.cs
+++ LaPoderosaApp2020/MainActivity.cs
@@ -10,47 +10,48 @@ namespace LaPoderosaApp2020
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        //Declaramos las variables que van a manipular a los controles
         Button btninicio, btnhistoria, btnmision;
+       
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.menumetro);
-            //Vinculamos las variables con los controles de la interfaz
-
+            //Vincular las variables con sus respectivos controles
             btninicio = FindViewById<Button>(Resource.Id.button1);
             btnhistoria = FindViewById<Button>(Resource.Id.button2);
             btnmision = FindViewById<Button>(Resource.Id.button3);
 
-            //Programar los eventos click
+            //Establecer el evento
             btninicio.Click += Btninicio_Click;
             btnhistoria.Click += Btnhistoria_Click;
-            btnmision.Click += Btnmision_Click;
+            btnmision.Click += Btnmision_Click;    
+
+
+
+
         }
 
         private void Btnmision_Click(object sender, System.EventArgs e)
         {
-            //el llamado a la activityInicio.
-
+            //Programar el llamado de la siguiente actividad
             Intent i = new Intent(this, typeof(ActivityMision));
             StartActivity(i);
         }
 
         private void Btnhistoria_Click(object sender, System.EventArgs e)
         {
-            //el llamado a la activityInicio.
-
+            //Programar el llamado de la siguiente actividad
             Intent i = new Intent(this, typeof(ActivityHistoria));
             StartActivity(i);
         }
 
         private void Btninicio_Click(object sender, System.EventArgs e)
         {
-            //el llamado a la activityInicio.
-
-            Intent i = new Intent(this,typeof(ActivityInicio));
+            //Programar el llamado de la siguiente actividad
+            Intent i = new Intent(this, typeof(ActivityInicio));
             StartActivity(i);
-            
         }
     }
 }
